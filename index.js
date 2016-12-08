c0fe789d698b2fc2db86bbd693306fb6653542a4
HomelessGruesomeMoose <HomelessGruesomeMoose@users.noreply.github.com>
2016-12-08 11:14:21 +0100

    making nice phrase

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-module.exports = function() {
+var d, num
+var ld = require('lodash);
 
+
+module.exports = function(objects) {
+    
+    var string = "hello"
+    
+    
+    for(i=0; i++)
+    {
+    if(objects[i].name.val.indexOf("day") > 0) {
+        string += '! Today is' +objects.name.val
+    }
+    
+     
+        string += ' and the time is ' + Date.now();
+        
+        
+        string += "welcome to the "+objects[i].world.type
+        
+        
+        return string
+    
+        
+    }
+    
     return "hello world";
 
 }
